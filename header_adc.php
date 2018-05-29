18552aa2da4309232497ecc4638d57ce37835698
crosswalk-tajima <tajima@crosswalk.co.jp>
2018-05-29 16:09:35 +0900

    Initial commit

---
+++ header_adc.php
@@ -0,0 +1,13 @@
+	<header>
+	<style type="text/css">header{height:100px;} header .wrap{padding:0 28px;}</style>
+	<div class="box_inner">
+		<div class="wrap">
+			<p class="logo"><a href="./"><img src="../../common/pc/img/header_logo02.png" alt="Goodモバイル" width="280" height="50"></a></p><!--
+		--><p class="logo"><img src="../../common/pc/img/header_lead.png" alt="【相談無料】お近くの店舗まで、お気軽にお電話ください！" width="644" height="50"></p><!--
+		--><a href="tel:0120949017"><img src="../../common/pc/img/header_tel_shinjyuku_adc.png" alt="東京 新宿店 0120949017" width="231" height="48"></a><!--
+		--><a href="tel:0120949243"><img src="../../common/pc/img/header_tel_shibuya_adc.png" alt="東京 渋谷店 0120949243" width="231" height="48"></a><!--
+		--><a href="tel:0120949307"><img src="../../common/pc/img/header_tel_osaka_adc.png" alt="大阪 日本橋店 0120949307" width="231" height="48"></a><!--
+		--><a href="tel:0120949495"><img src="../../common/pc/img/header_tel_namba_adc.png" alt="大阪 なんば店 0120949495" width="231" height="48"></a>
+		</div>
+	</div>
+	</header>
---
+++ shop_adc.php
@@ -0,0 +1,64 @@
+			<dl class="infoarea">
+				<dt class="shopname">東京 新宿店</dt>
+				<dd class="shoptext">
+					<dl>
+						<dt class="subttl">所在地</dt>
+						<dd><p>〒160-0023<br>東京都新宿区西新宿7-1-4  ゼンコービル4F</p>
+						<p><a class="btn" href="https://goo.gl/maps/mdq2l" target="_blank"><img src="img/access_ico01.png" alt="" width="24" height="24">MAPを見る</a></p></dd>
+						<dt class="subttl">最寄駅</dt>
+						<dd><p>都営大江戸線「新宿西口駅」徒歩1分<br>東京メトロ「新宿駅」徒歩2分<br>JR「新宿駅」西武西鉄「西武新宿駅」徒歩3分</p></dd>
+						<dt class="subttl">TEL</dt>
+						<dd><p><a class="btn tel" href="tel:0120949017"><img src="img/access_ico02.png" alt="" width="17" height="24">0120-949-017</a></p></dd>
+						<dt class="subttl">営業時間</dt>
+						<dd><p>10：00～23：00(定休日なし)</p></dd>
+					</dl>
+				</dd>
+			</dl>
+			<dl class="infoarea">
+				<dt class="shopname">東京 渋谷店</dt>
+				<dd class="shoptext">
+					<dl>
+						<dt class="subttl">所在地</dt>
+						<dd><p>〒150-0042<br>東京都渋谷区宇田川町28-3　渋谷モーリスビル6F</p>
+						<p><a class="btn" href="https://goo.gl/maps/tXjfa" target="_blank"><img src="img/access_ico01.png" alt="" width="24" height="24">MAPを見る</a></p></dd>
+						<dt class="subttl">最寄駅</dt>
+						<dd><p>JR「渋谷駅」ハチ公出口 徒歩3分<br>京王電鉄「渋谷駅」徒歩7分</p></dd>
+						<dt class="subttl">TEL</dt>
+						<dd><p><a class="btn tel" href="tel:0120949243"><img src="img/access_ico02.png" alt="" width="17" height="24">0120-949-243</a></p></dd>
+						<dt class="subttl">営業時間</dt>
+						<dd><p>10：00～23：00(定休日なし)</p></dd>
+					</dl>
+				</dd>
+			</dl>
+			<dl class="infoarea">
+				<dt class="shopname">大阪 日本橋店</dt>
+				<dd class="shoptext">
+					<dl>
+						<dt class="subttl">所在地</dt>
+						<dd><p>〒556-0005<br>大阪府大阪市浪速区日本橋4丁目7番22号<br>シマムセンビル1F 101号<br>※お車でお越しのお客様は目の前が時間決め駐車場となっておりますので、お気軽にお立ち寄り下さい。</p>
+						<p><a class="btn" href="https://goo.gl/maps/nLLVT" target="_blank"><img src="img/access_ico01.png" alt="" width="24" height="24">MAPを見る</a></p></dd>
+						<dt class="subttl">最寄駅</dt>
+						<dd><p>大阪市営地下鉄堺筋線「恵美須町駅」徒歩4分</p></dd>
+						<dt class="subttl">TEL</dt>
+						<dd><p><a class="btn tel" href="tel:0120949307"><img src="img/access_ico02.png" alt="" width="17" height="24">0120-949-307</a></p></dd>
+						<dt class="subttl">営業時間</dt>
+						<dd><p>11：00～20：00(定休日なし)</p></dd>
+					</dl>
+				</dd>
+			</dl>
+			<dl class="infoarea">
+				<dt class="shopname">大阪 なんば店</dt>
+				<dd class="shoptext">
+					<dl>
+						<dt class="subttl">所在地</dt>
+						<dd><p>〒542-0076<br>大阪市中央区難波4-5-14　コックドールビル6F</p>
+						<p><a class="btn" href="https://goo.gl/maps/gogJY" target="_blank"><img src="img/access_ico01.png" alt="" width="24" height="24">MAPを見る</a></p></dd>
+						<dt class="subttl">最寄駅</dt>
+						<dd><p>大阪市営地下鉄[御堂線][四ツ橋線][千日前線]「なんば駅」徒歩0分<br>南海電鉄「なんば駅」徒歩0分<br>※地下9番出口に直通エレベーターがございます</p></dd>
+						<dt class="subttl">TEL</dt>
+						<dd><p><a class="btn tel" href="tel:0120949495"><img src="img/access_ico02.png" alt="" width="17" height="24">0120-949-495</a></p></dd>
+						<dt class="subttl">営業時間</dt>
+						<dd><p>9：00～20：00(定休日なし)</p></dd>
+					</dl>
+				</dd>
+			</dl>
